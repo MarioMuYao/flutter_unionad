96329b86bc2e3dca30f6c62a763afe2b845935ad
Mario <>
2024-07-30 09:27:02 +0800

    优化

--- android/src/main/kotlin/com/gstory/flutter_unionad/nativead/NativeAdView.kt
+++ android/src/main/kotlin/com/gstory/flutter_unionad/nativead/NativeAdView.kt
@@ -65,14 +65,7 @@ class NativeAdView(
             .setCodeId(mCodeId)
             .setSupportDeepLink(supportDeepLink!!)
             .setAdCount(1) //请求广告数量为1到3条
-            .setExpressViewAcceptedSize(
-                UIUtils.px2dip(context, viewWidth),
-                UIUtils.px2dip(context, viewHeight)
-            )
-            .setImageAcceptedSize(
-                UIUtils.dip2px(context, viewWidth).toInt(),
-                UIUtils.dip2px(context, viewHeight).toInt()
-            )
+            .setExpressViewAcceptedSize(viewWidth, viewHeight)
             .build()
         val mTTAdNative = TTAdSdk.getAdManager().createAdNative(activity)
         //加载广告
