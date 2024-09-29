f8766fe65a346c87172b3e0e56096fea2d84d449
Mario <>
2024-09-29 15:07:40 +0800

    添加日志

--- android/src/main/kotlin/com/gstory/flutter_unionad/nativead/NativeAdView.kt
+++ android/src/main/kotlin/com/gstory/flutter_unionad/nativead/NativeAdView.kt
@@ -119,6 +119,7 @@ class NativeAdView(
             override fun onRenderSuccess(view: View?, width: Float, height: Float) {
                 mContainer?.removeAllViews()
                 mContainer?.addView(view)
+                Log.e(TAG, "ExpressView width:" + view?.layoutParams.width + " height:" + view?.layoutParams.height)
                 var map: MutableMap<String, Any?> =
                         mutableMapOf("width" to width, "height" to height)
                 channel?.invokeMethod("onShow", map)
